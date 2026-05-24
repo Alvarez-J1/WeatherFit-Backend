4a2399828eba74196eb59d4d6f68d71cbf1973ee
Joel Alvarez <Joel124@outlook.es>
2026-05-24 16:42:17 -0400

    fixed bugs

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,7 @@
-const { JWT_SECRET = "some-very-strong-key", MONGO_URL } = process.env;
+const {
+  JWT_SECRET = "some-very-strong-key",
+  MONGO_URL = "mongodb://127.0.0.1:27017/wtwr",
+} = process.env;
 
 module.exports = {
   JWT_SECRET,
