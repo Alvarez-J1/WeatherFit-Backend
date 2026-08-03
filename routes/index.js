510dd2adf0dba1e0120051b49e829ef2476a1ad1
Joel Alvarez <Joel124@outlook.es>
2026-08-03 06:16:13 -0400

    fix: validate signin requests

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const itemRouter = require("./clothingItems");
 
 const { createUser, login } = require("../controllers/users");
 
+const { validateAuthentication } = require("../middlewares/validation");
+
 const userRouter = require("./users");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
-router.post("/signin", login);
+router.post("/signin", validateAuthentication, login);
 router.post("/signup", createUser);
 module.exports = router;
