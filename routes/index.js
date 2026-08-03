868bb96fbddc28af6c5b0bb2f48988df70bf3b92
Joel Alvarez <Joel124@outlook.es>
2026-08-03 06:16:48 -0400

    fix: validate signup requests

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,15 @@ const itemRouter = require("./clothingItems");
 
 const { createUser, login } = require("../controllers/users");
 
-const { validateAuthentication } = require("../middlewares/validation");
+const {
+  validateAuthentication,
+  validateUserInfoBody,
+} = require("../middlewares/validation");
 
 const userRouter = require("./users");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
 router.post("/signin", validateAuthentication, login);
-router.post("/signup", createUser);
+router.post("/signup", validateUserInfoBody, createUser);
 module.exports = router;
