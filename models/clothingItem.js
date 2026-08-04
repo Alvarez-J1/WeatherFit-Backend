0a060ec04b64ceda518725243a819cfe02b033b7
Joel Alvarez <Joel124@outlook.es>
2026-08-04 06:47:29 -0400

    fix: trim clothing image urls

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -18,6 +18,7 @@ const clothingItemSchema = new mongoose.Schema({
   imageUrl: {
     type: String,
     required: true,
+    trim: true,
     validate: {
       validator(value) {
         return validator.isURL(value, {
