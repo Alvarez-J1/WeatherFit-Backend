3d3a593ead92f36e3e3f5180717208a9d9794cad
Joel Alvarez <Joel124@outlook.es>
2026-08-04 06:50:21 -0400

    refactor: route auth errors through next

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    throw new UnauthorizedError("Authorization required");
+    return next(new UnauthorizedError("Authorization required"));
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -17,7 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    throw new UnauthorizedError("Authorization required");
+    return next(new UnauthorizedError("Authorization required"));
   }
   req.user = payload;
   return next();
