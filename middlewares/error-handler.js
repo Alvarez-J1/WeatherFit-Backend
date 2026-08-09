628cfef5c0d718e7856891e14a72691fc839761f
Joel Alvarez <Joel124@outlook.es>
2026-08-09 01:00:39 -0400

    fix: fallback missing error messages

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,9 +1,13 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
   const { statusCode = 500, message } = err;
+  const responseMessage =
+    statusCode === 500
+      ? "An error occurred on the server"
+      : message || "An error occurred";
 
   res.status(statusCode).send({
-    message: statusCode === 500 ? "An error occurred on the server" : message,
+    message: responseMessage,
   });
 };
 
