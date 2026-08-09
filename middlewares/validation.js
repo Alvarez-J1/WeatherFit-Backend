d205ff3bd60079acecc89249684cf967e32ab244
Joel Alvarez <Joel124@outlook.es>
2026-08-09 00:15:39 -0400

    fix: require signup name field

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -40,9 +40,10 @@ const validateClothingItemBody = celebrate({
 
 const validateUserInfoBody = celebrate({
   [Segments.BODY]: Joi.object({
-    name: Joi.string().min(2).max(30).messages({
+    name: Joi.string().required().min(2).max(30).messages({
       "string.min": 'The minimum length of the "name" field is 2',
       "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
     }),
     avatar: Joi.string().required().custom(validateURL).messages({
       "string.empty": 'The "avatar" field must be filled in',
